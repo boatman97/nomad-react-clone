fa825c8e9fb28b5df4b3edf4931e7b66d4ac63a1
Woogie <test@gmail.com>
2020-09-15 22:07:09 +0900

    Updates

---
+++ precache-manifest.7aadfc9c393c212cc3ffccf2f0aa11ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa810ea4eb50faf5585e268a7f3bf89",
+    "url": "/nomad-react-clone/index.html"
+  },
+  {
+    "revision": "522f4ae362fcc6c7af30",
+    "url": "/nomad-react-clone/static/css/main.12e6be58.chunk.css"
+  },
+  {
+    "revision": "11f4fe2205dd64b315cc",
+    "url": "/nomad-react-clone/static/js/2.df258bfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nomad-react-clone/static/js/2.df258bfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "522f4ae362fcc6c7af30",
+    "url": "/nomad-react-clone/static/js/main.b156496d.chunk.js"
+  },
+  {
+    "revision": "985a7837866c41866619",
+    "url": "/nomad-react-clone/static/js/runtime-main.f4fbc298.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nomad-react-clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
